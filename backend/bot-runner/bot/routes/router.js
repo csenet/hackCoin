13458081c718709893e5516127bfa56a1b74f475
csenet <kouichi.hirachi@gmail.com>
2022-01-29 08:50:17 +0000

    title変更機能追加

--- backend/bot-runner/bot/routes/router.js
+++ backend/bot-runner/bot/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Controller
-const {showRanking} = require("../services/cronWorker");
+const { showRanking } = require("../services/cronWorker");
 
 router.get('/ping', (req, res) => {
   res.send("OK!")
@@ -10,15 +10,14 @@ router.get('/ping', (req, res) => {
 
 router.get('/cron/ranking/:dev', async (req, res) => {
   const isDev = req.params.dev === "dev" ? true : false;
-  res.json(req.params);
-  // const apiKey = req.headers['x-api-key']
-  // if (apiKey != 'a9eb03b0-0d3a-4a40-9d09-0bdc6b560a14') {
-  //   res.status(401).json({message: "Un authorised"})
-  //   return
-  // }
-  // const title = req.body.title;
-  // showRanking(isDev,title);
-  // res.json({message: isDev})
+  const apiKey = req.headers['x-api-key']
+  if (apiKey != 'a9eb03b0-0d3a-4a40-9d09-0bdc6b560a14') {
+    res.status(401).json({ message: "Un authorised" })
+    return
+  }
+  const title = req.query.title;
+  showRanking(isDev, title);
+  res.json({ message: isDev })
 })
 
 module.exports = router;
